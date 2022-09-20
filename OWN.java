e7d3ce231546f8dccb44e08d34197fe55f2183fa
KaRTHiKNaughtyboy <113936696+KaRTHiKNaughtyboy@users.noreply.github.com>
2022-09-20 22:11:25 +0530

    Update OWN.java

--- OWN.java
+++ OWN.java
@@ -1,7 +1,7 @@
 public class OWN {
     public static void main(String args[]) {
         int a = 110;
-        int b = 20;
+        int b = 2000;
         int c = a * b;
         System.out.println(c);
 
